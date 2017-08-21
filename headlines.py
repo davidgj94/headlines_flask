26df4581c6c7fe5d34eb6919e67fc7989b1e9b09
Ｄａｖｉｄ <davidgj1994@gmail.com>
2017-08-21 13:51:25 +0200

    srefactor successfully

--- headlines.py
+++ headlines.py
@@ -13,29 +13,42 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+DEFAULTS = {'publication':'bbc',
+            'city': 'London,UK'}
+
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=f5756e2b53c3a0b1e11d5315dfd644c6"
+
+@app.route("/")
+def home():
+  # get customized headlines, based on user input or default
+  publication = request.args.get('publication')
+  if not publication:
+      publication = DEFAULTS['publication']
+  articles = get_news(publication)
+  # get customized weather based on user input or default
+  city = request.args.get('city')
+  if not city:
+      city = DEFAULTS['city']
+  weather = get_weather(city)
+  return render_template("home.html", articles=articles,weather=weather)
+
+def get_news(query):
+  if query.lower() not in RSS_FEEDS:
+      publication = DEFAULTS["publication"]
+  else:
+      publication = query.lower()
+  feed = feedparser.parse(RSS_FEEDS[publication])
+  return feed['entries']
+
 def get_weather(query):
-  api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=f5756e2b53c3a0b1e11d5315dfd644c6"
   query = urllib.quote(query)
-  url = api_url.format(query)
+  url = WEATHER_URL.format(query)
   data = urllib2.urlopen(url).read()
   parsed = json.loads(data)
   weather = None
-  if parsed.get("weather"):
-      weather = {"description":parsed["weather"][0]["description"],
-      "temperature":parsed["main"]["temp"],
-      "city":parsed["name"]}
+  if parsed.get('weather'):
+      weather = {'description':parsed['weather'][0]['description'],'temperature':parsed['main']['temp'],'city':parsed['name']}
   return weather
-
-@app.route("/")
-def get_news():
-  query = request.args.get("publication")
-  if not query or query.lower() not in RSS_FEEDS:
-    publication = "bbc"
-  else:
-    publication = query.lower()
-  feed = feedparser.parse(RSS_FEEDS[publication])
-  weather = get_weather("London,UK")
-  return render_template("home.html",articles=feed['entries'], weather=weather)
 
 if __name__ == "__main__":
   app.run(port=5000, debug=True)
